15a0f8369127d084fc739c12e424402243d9cf37
JottaMiranda <jottam.dev@gmail.com>
2026-04-07 09:06:30 -0600

    backend de reportes

--- src/routes/v1/admin/index.js
+++ src/routes/v1/admin/index.js
@@ -3,6 +3,7 @@ import adminCitasRoutes from "./citas.js";
 import empleadosRoutes from "./empleados.js";
 import personasRoutes from "./personas.js";
 import plansRoutes from "./plans.js";
+import reportesRoutes from "./reportes.js";
 import sucursalesRoutes from "./sucursales.js";
 
 export default async function adminRoutes(app) {
@@ -12,4 +13,5 @@ export default async function adminRoutes(app) {
   await app.register(empleadosRoutes, { prefix: "/empleados" });
   await app.register(sucursalesRoutes, { prefix: "/sucursales" });
   await app.register(adminCitasRoutes, { prefix: "/citas" });
+  await app.register(reportesRoutes, { prefix: "/reportes" });
 }
